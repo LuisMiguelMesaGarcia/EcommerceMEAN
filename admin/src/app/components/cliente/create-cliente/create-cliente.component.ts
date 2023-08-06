3a168c6f325a24e92b0a9a467970f5e7781be6a5
Miguel Mesa <luismg1032@gmail.com>
2023-08-06 18:31:06 -0500

    Redirecciones y arreglos

--- admin/src/app/components/cliente/create-cliente/create-cliente.component.ts
+++ admin/src/app/components/cliente/create-cliente/create-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AdminService } from 'src/app/services/admin.service';
 import { ClienteService } from 'src/app/services/cliente.service';
 declare var iziToast:any;
@@ -14,7 +15,7 @@ export class CreateClienteComponent {
   }; //este objeto se llena en el HTML con el ngModule
   public token:any;
 
-  constructor(private _clienteService:ClienteService, private _adminService: AdminService){
+  constructor(private _clienteService:ClienteService, private _adminService: AdminService,private _router: Router){
     this.token=_adminService.getToken();
   }
 
@@ -28,7 +29,21 @@ export class CreateClienteComponent {
     if(registroForm.valid){
       this._clienteService.registroClienteAdmin(this.cliente,this.token).subscribe(
         response=>{
-          console.log(response);
+          iziToast.show({
+            title: 'Success',
+            position:'topRight',
+            message: 'Se registró correctamente el nuevo cliente',
+            titleColor:'#1DC74C'
+          });
+          this.cliente={
+            genero:'',
+            nombre:'',
+            apellido:'',
+            f_nacimiento:'',
+            telefono:'',
+            email:'',
+          }
+          this._router.navigate(['/panel/clientes'])
         },error=>{
           console.log(error);
         }
